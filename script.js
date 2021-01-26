fc1c7913b43a61b33d8bc789d219c77bc2c6d1d5
Lucas <lucas.moquet95@gmail.com>
2021-01-26 18:54:07 +0100

    debut js

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
-window.onload = function() {
-
-    var furniture = new XMLHttpRequest();
-    var furnitureResult = document.querySelector("#test");
+/* const { response } = require("express");
+ */
+//Recupérer API 
+    let furnitures;
+    let furnitureResult = document.querySelector(".row-cards");
     console.log(furnitureResult);
-    furniture.onreadystatechange = function() {
-        if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-            var response = JSON.parse(this.responseText);
-            console.log(response.productId);
-            furnitureResult.innerHTML = response.objectId ;
-        }
-    };
-    furniture.open("GET", "localhost:3000/api/furniture");
-    furniture.send();
 
-}
+    const fetchFurniture = async() => {
+        furnitures = await fetch("http://localhost:3000/api/furniture")
+        .then(response => response.json());
+        console.log(furnitures);
+    }
+    
+    fetchFurniture();
+  
+    const showFurnitures = async() => {
+        await fetchFurniture();
+        console.log ("ok")
+        furnitureResult.innerHTML = (
+            furnitures
+                .filter(furniture => furniture.name.toLowerCase())
+                .map(furniture => (
+                    `
+                    <div class="col-12 col-lg-4">
+                        <div class="card furniture">
+                            <img src="${furniture.imageUrl}" alt="meuble en chêne" class="card-img-top furniture__img">
+                            <div class="card-body" id="test">
+                                <p class="card-title furniture__name" id="name">${furniture.name}</p>
+                                <p class="card-text furniture__varnish" id="varnish">${furniture.varnish}</p>
+                                <p class="card-text furniture__description" id="description">${furniture.description}</p>
+                                <p class="card-text furniture__price" id="test">${furniture.price} €</p>
+                            </div>
+                        </div>
+                    </div> 
+                    `
+                    )).join("")
+        
+        );
+    };
+
+    showFurnitures()
